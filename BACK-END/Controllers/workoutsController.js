baedcc41d776da13b23d3f752ba75bf7b9f362a0
aditya-goel <adityagoel0320@gmail.com>
2023-04-29 02:08:52 +0530

    29/04/23

--- BACK-END/Controllers/workoutsController.js
+++ BACK-END/Controllers/workoutsController.js
@@ -1,4 +1,4 @@
-// made this file as we want diff fnc to bulid here than impoirt directly in routesfile as to clean code
+// made this file as we want diff fnc to bulid here than impoirt directly in routesfile as to make clean code
 
 
 let Workout = require("../model/WorkoutsModel")
@@ -8,7 +8,6 @@ let mongoose = require("mongoose")
 // to create a new workout
 let createWorkout = async (req, res) => {
     let { title, load, reps } = req.body;
-
     try {
         let workout = await Workout.create({ title, reps, load })
         res.status(200).json(workout)
@@ -31,31 +30,28 @@ let getWorkouts = async (req, res) => {
     }
 }
 
+
+
+
 // to get one workout
 let getSingleWorkout = async (req, res) => {
+    let { id } = req.params;
+    let workout = await Workout.findById(id);
 
-    if (!mongoose.Types.objectId.isValid(id)) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({
             error: "id is incorrect"
         })
     }
 
+    // to get that    particulat id to display that workout
 
-
-    // to get that particulat id to display that workout
-    let { id } = req.params;
-    try {
-        let workout = await Workout.findById(id);
-        if (!workout) {
-            return res.status(404).json({ error: "cannot find that id" })
-        }
-        return res.status(200).json(workout)
-
-
+    if (!workout) {
+        return res.status(404).json({ error: "cannot find that id" })
     }
-    catch (error) {
-        console.log(error);
-    }
+    return res.status(200).json(workout)
+
+
 }
 
 
@@ -63,27 +59,25 @@ let getSingleWorkout = async (req, res) => {
 // to delete
 
 let D = async (req, res) => {
+    let { id } = req.params;
+
+
     // ac check that if id give is wrong
-    if (!mongoose.Types.objectId.isValid(id)) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({
             error: "id is incorrect"
         })
     }
 
     // to get that particulat id to display that workout
-    let { id } = req.params;
-    try {
-        let workout = await Workout.findOneAndDelete({ _id: id });
-        if (!workout) {
-            return res.status(404).json({ error: "cannot find that id" })
-        }
-        return res.status(200).json(workout)
+    let workout = await Workout.findOneAndDelete({ _id: id });
+    if (!workout) {
+        return res.status(404).json({ error: "cannot find that id" })
+    }
+    return res.status(200).json(workout)
 
 
-    }
-    catch (error) {
-        console.log(error);
-    }
+
 
 }
 
@@ -91,27 +85,24 @@ let D = async (req, res) => {
 // to update a workout
 
 let updateWorkout = async (req, res) => {
+    let { id } = req.params;
+
+
     // ac check that if id give is wrong
-    if (!mongoose.Types.objectId.isValid(id)) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({
             error: "id is incorrect"
         })
     }
 
     // to get that particulat id to display that workout
-    let { id } = req.params;
-    try {
-        let workout = await Workout.findOneAndUpdate({ _id: id }, { ...req.body });
-        if (!workout) {
-            return res.status(404).json({ error: "cannot find that id" })
-        }
-        return res.status(200).json(workout)
+    let workout = await Workout.findOneAndUpdate({ _id: id }, { ...req.body });
+    if (!workout) {
+        return res.status(404).json({ error: "cannot find that id" })
+    }
+    return res.status(200).json(workout)
 
 
-    }
-    catch (error) {
-        console.log(error);
-    }
 
 }
 
