81a249e83721be95236236ba15ab46fdcdced41d
aditya-goel <adityagoel0320@gmail.com>
2023-04-30 01:19:38 +0530

    29/04/23

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [workouts, setworkouts] = useState(null)
+
+
+  useEffect(() => {
+
+    let fetchworkouts = async () => {
+
+      let response = await fetch('/api/workouts')
+      let json = response.json();
+
+      setworkouts(json)
+      console.log(workouts)
+      // if (response.ok) {
+      //   setworkouts(json)
+      // }
+    }
+    fetchworkouts();
+  }, [])
+
   return (
-    <div>Home</div>
+    <>
+      <div>Home</div>
+
+      <div className="workouts">
+
+        {
+          workouts && workouts.map((workout) => {
+            return (
+              <p key={workout._id}>{workout.title}</p>
+            )
+          })
+        }
+      </div>
+    </>
   )
 }
